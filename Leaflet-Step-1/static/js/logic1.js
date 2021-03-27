8729948362792465bba265679f489624a97987b7
skunnath2020 <sskunnath@gmail.com>
2021-03-26 21:10:11 -0400

    Code complete

--- Leaflet-Step-1/static/js/logic1.js
+++ Leaflet-Step-1/static/js/logic1.js
@@ -1,17 +1,18 @@
 var usgs_url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-//var tectonicPlatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+var plates_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
 //Associates the data link
 d3.json(usgs_url, function(data) {
     console.log(data);
     // Once we get a response, send the data.features object to the createFeatures function
-    console.log("Before CF");
-    createFeatures(data.features);
-    console.log("After Create Features");
-  });
+   createFeatures(data.features);
+   });
+d3.json(plates_url, function(response){
 
+})
 function createFeatures(earthquakeData) {
       //loop through the data
-    console.log(earthquakeData)
+    var earthquakeMarkers = []
     EarthQuakeLoc = earthquakeData.map((feature) => 
       L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
         radius: getRadius(feature.properties.mag),
@@ -24,8 +25,10 @@ function createFeatures(earthquakeData) {
         fillOpacity: 0.5   
     }).bindPopup("<h3>" + feature.properties.place +
     "</h3><hr><p>" + "<h3>"+new Date(feature.properties.time)+"</h3>" + "</p><hr><h3>Magnitude: " + feature.properties.mag + "</h3>").addTo(myMap));
-    };
+    
+   }
   //function createMap(earthQuakes) {
+  // Define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
       tileSize: 512,
@@ -34,11 +37,39 @@ function createFeatures(earthquakeData) {
       id: "mapbox/streets-v11",
       accessToken: API_KEY
     });
+    
+    var grayscale = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.light",
+      accessToken: config.API_KEY
+    });
+
+    var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.outdoors",
+      accessToken: config.API_KEY
+    });
+  
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+      "streetmap": streetmap,
+      "Grayscale": grayscale,
+      "Outdoors": outdoors
+    };
+  
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+      "Fault Lines": plates
+      // Earthquakes: earthQuakes
+    };
   
   // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("mapid", {
       center: [39.106667, -94.676392],
-      zoom: 5 
+      zoom: 5,
+      layers: [satellite, plates, earthQuakes]
     });
     console.log("After myMap & before attaching streetMap");
     streetmap.addTo(myMap);
@@ -57,8 +88,14 @@ function createFeatures(earthquakeData) {
       return div;
     };
 
+  //   legend.onAdd = function () {
+  //     var div = L.DomUtil.create('div', 'legend');
+  //     return div;
+  //   }; 
+ 
     legend.addTo(myMap);
-    
+    // document.querySelector(".legend").innerHTML=displayLegend();
+  //}
 
   function getRadius(magnitude){
         if (magnitude <= 1){
@@ -78,8 +115,3 @@ function createFeatures(earthquakeData) {
       
      return color;
     }
-
-
-
-
-
